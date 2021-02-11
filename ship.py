b0a725079ddd347240d0dc63828230648638c6b0
Niclas Behrendt <niclas.behrendt@msg.group>
2021-02-11 16:09:55 +0100

    added up and down movement

--- ship.py
+++ ship.py
@@ -20,10 +20,13 @@ class Ship:
 
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         # Movement flag
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on movement flags."""
@@ -31,8 +34,13 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
 
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
