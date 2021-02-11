1e09cf5853f9826dc1c704c856f98844c210ec1a
Niclas Behrendt <niclas.behrendt@msg.group>
2021-02-11 19:07:06 +0100

    added bullet fire

--- settings.py
+++ settings.py
@@ -3,6 +3,13 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (199, 10, 10)
+        self.bullets_allowed = 5
+
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
